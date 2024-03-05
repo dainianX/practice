2d08f12f6a98a418b6f57b2bdc67dd12c2a414df
dainianx <dainianx@gmail.com>
2024-03-05 23:16:42 +0800

    Completed 'sorting before deleting all repetitive numbers'

--- C++/Algorithm Design/Second/009/main.cpp
+++ C++/Algorithm Design/Second/009/main.cpp
@@ -13,14 +13,23 @@ int main() {
     }
     sort(numbers.begin(), numbers.end());
 
-    int size = numbers.size();
-    for (int i = 0; i < n - 1; i++) {
+    int size = static_cast <int> (numbers.size());
+    for (int i = 0; i < size - 1; ) {
         if (numbers[i] == numbers[i + 1]) {
-            numbers.erase(numbers.begin() + i);
-            size--; //更新数组大小
-            i--; //删除了一个元素，更新当前位置
+            // 找到重复的数字的范围
+            int j = i + 1;
+            while (j < size && numbers[i] == numbers[j]) {
+                j++;
+            }
+
+            // 删除重复的数字，但只保留一个
+            numbers.erase(numbers.begin() + i + 1, numbers.begin() + j);
+            size -= (j - i - 1); // 更新数组大小
+        } else {
+            i++;
         }
     }
+
     cout << size << endl;
     for (auto num : numbers) {
         cout << num << ' ';
