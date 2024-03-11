32084f6f44f72230c1a7d94003358390977ec49f
dainianx <dainianx@gmail.com>
2024-03-11 18:19:50 +0800

    Done with C# homework week 2

---
+++ C#/MergeArray/MergeArray/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] Merge(int[] a1, int[] a2) {
+	int[] a3 = new int[a1.Length + a2.Length];
+	for (int i = 0; i < a1.Length; i++)
+	{
+		a3[i] = a1[i];
+	}
+	for (int j = 0; j < a2.Length; j++)
+	{
+		a3[j + a1.Length] = a2[j];
+	}
+	;
+	Array.Sort(a3);
+	return a3;
+}
+
+//int[] a1 = { 1, 4, 7, 12, 20 };
+//int[] a2 = {10,15,17,33};
+//int[] a1 = { 2,3,5,7 };
+//int[] a2 = {11,13,17};
+//int[] a1 = { 2,3,5,7,11 };
+//int[] a2 = {7,11,13,17};
+int[] a1 = { 1, 2, 3 };
+int[] a2 = { };
+
+int[] a3 = Merge(a1, a2);
+foreach (int i in a3) {
+	Console.Write(i + "  ");
+}
--- C++/Practice/Test.cpp
+++ C++/Practice/Test.cpp
@@ -3,10 +3,41 @@
 //
 #include<iostream>
 using namespace std;
+float temp;
+float area1(float r) {
+    temp = r * r;
+    return temp;// 返回temp的值，右值
+}
 
+float& area2(float r) {
+    temp = r * r;
+    return temp; //返回temp本身，左值
+}
+
+void print(int x = 3) {
+
+}
+void print(char* s1="hello", char* s2) {
+
+}
 int main() {
-    int x = 1;
-    int * const px = &x;
-    px = nullptr;
-    cout << x;
+    float a1 = area1(5.0);
+    float& a2 = area1(5.0);
+    float& a3 = area2(5.0);
+    float a4 = area1(4) = 10;
+    float a5 = area2(4) = 20;
+    char *pstr {"c++"};
+    const char* str1{"Java"};;
+    char str2[]{"python"};
+    const char *const ps{str2};
+    str2[4] = 'e';
+    ps = str2;
+    ps[1] = 'o';
+    pstr = str1; //
+    str1[1] = '0';
+    pstr[1]= pstr[2] = 'p';
+    print(5);
+    print();
+    print("world");
+
 }
